d7dd228dbb397e9574b79ce6952a1d7d29b81992
Joe Gatling <joe@nightschoolstudio.com>
2021-03-04 22:45:24 -0800

    Added toggle inspector lock handler

---
+++ Assets/Package/Scripts/Editor/ButtonHandlers/ToggleInspectorLockButtonHandler.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+namespace JoeGatling.ButtonGrids.ButtonHandlers
+{    
+    [System.Serializable]
+    public class ToggleInspectorLockButtonHandler : IButtonHandler
+    {
+        private GlowingButton _button = null;
+
+        ButtonGrids.LedFunctions.ILedFunction _defaultLedFunction = null;
+
+        public void Initialize(GlowingButton button)
+        {
+            Teardown();
+
+            _defaultLedFunction = new LedFunctions.LedDelegate(() =>
+            {
+                return UnityEditor.ActiveEditorTracker.sharedTracker.isLocked && LedFunctions.LedFlashing.fast.GetLedState(button);
+            });
+
+            _button = button;
+            _button.onKeyStateChanged += OnButtonStateChanged;
+            _button.ledFunction = _defaultLedFunction;
+        }
+
+        public void Teardown()
+        {
+            if(_button != null)
+            {
+                _button.onKeyStateChanged -= OnButtonStateChanged;
+                _button.ledFunction = null;
+                _button = null;
+            }
+        }
+
+        public void Update()
+        { }           
+
+
+        protected void OnButtonStateChanged(bool state)
+        {
+            if(state == true)
+            {
+                UnityEditor.ActiveEditorTracker.sharedTracker.isLocked = !UnityEditor.ActiveEditorTracker.sharedTracker.isLocked;
+            }
+        }
+    }
+}
